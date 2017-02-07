5cac0e60c7b65bfe015c10a7d31644d8328e6f67
teQoo <anes2354@gmail.com>
2017-02-07 18:21:24 +0900

    17.02.07

--- MemberEx/src/com/zyde/board/BoardService.java
+++ MemberEx/src/com/zyde/board/BoardService.java
@@ -1,21 +1,23 @@
-package dice;
+package com.zyde.board;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class DiceServlet
+ * Servlet implementation class BoardService
  */
-public class DiceServlet extends HttpServlet {
+@WebServlet("/BoardService")
+public class BoardService extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public DiceServlet() {
+    public BoardService() {
         super();
         // TODO Auto-generated constructor stub
     }
--- zyde/src/main/java/board/BoardServlet.java
+++ zyde/src/main/java/board/BoardServlet.java
@@ -1,23 +1,24 @@
-package weather;
+package board;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class MainWeather
+ * Servlet implementation class BoardServlet
  */
-public class MainWeather extends HttpServlet {
+@WebServlet(urlPatterns = "/board")
+public class BoardServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public MainWeather() {
+    public BoardServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -26,15 +27,14 @@ public class MainWeather extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher rd=request.getRequestDispatcher("/index.jsp");
-		rd.forward(request, response);
+		// TODO Auto-generated method stub
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		// TODO Auto-generated method stub
 	}
 
 }
