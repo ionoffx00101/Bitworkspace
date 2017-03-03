84ae5b13c24d1de5cf50faf669a06ceda2dcdcd6
teQoo <anes2354@gmail.com>
2017-03-03 11:25:57 +0900

    commit in Eclipse.

---
+++ git_test_iy0318/src/com/bitacademy/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.bitacademy.git;
+
+public class HelloGit
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("ㅎㅇㅎㅇ Git");
+		System.out.println("오늘은 3월 3일이야");
+	}
+
+}
